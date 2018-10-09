f0b6c0a3713cebc47749aba3e7f0cc828892b4ac
zawlinnnaing <zawlinnnaing0018@gmail.com>
2018-10-09 19:53:36 +0630

    resolved conflict (step.py)

--- step.py
+++ step.py
@@ -1,7 +1,7 @@
 from tkinter import *
 from tkinter import ttk
 
-class Steps(Frame):
+class Steps(Frame):x
 
     def __init__(self,master=NONE):
         # Frame.__init__(self,master)
@@ -77,12 +77,9 @@ class Steps(Frame):
             print (' '+ x.get())
             x.set(NONE)
 
-<<<<<<< HEAD
 root =Tk()
 root.title('Result for voted photos')
 # root.geometry('1600x800+0+0')
 root.attributes('-fullscreen',True)
 app = Steps(root)
 root.mainloop()
-=======
->>>>>>> 4422760a34ab71a0b6ae07a7494c39850ff19619
