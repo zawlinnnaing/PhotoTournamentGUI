950c717e689a8ac2146029ebee15b9a00dce85f9
zawlinnnaing <zawlinnnaing0018@gmail.com>
2018-10-09 20:00:24 +0630

    resolved conflict (step.py) #1

--- step.py
+++ step.py
@@ -77,9 +77,3 @@ class Steps(Frame):x
             print (' '+ x.get())
             x.set(NONE)
 
-root =Tk()
-root.title('Result for voted photos')
-# root.geometry('1600x800+0+0')
-root.attributes('-fullscreen',True)
-app = Steps(root)
-root.mainloop()
